8aed883f92e4c824990048371b3f978bdc4263d6
Luis fernando JU <luigfmp@gmail.com>
2024-09-21 13:10:51 -0500

    test: :test_tube: test code

--- src/app/api/transform/image/route.ts
+++ src/app/api/transform/image/route.ts
@@ -1,7 +1,10 @@
+import Image from '@/db/models/Image'
+import cloudinary from '@/shared/cloudinaryConfig'
 import { NextResponse } from 'next/server'
 
 import { ITransformFormData } from './responseCld.type'
 import { uploadCloudinaryBase64 } from './utils/cld'
+import { imageQuality } from './utils/qualitiesTransform'
 
 export async function POST(req: Request) {
   try {
@@ -10,28 +13,28 @@ export async function POST(req: Request) {
 
     const arrayBuffer = await data.image.arrayBuffer()
     const saveImage = await uploadCloudinaryBase64({ author: data.author, arrayBuffer })
-    // const temporalUrlTransformFile = cloudinary.url(saveImage.public_id, {
-    //   transformation: [...imageQuality]
-    // })
+    const temporalUrlTransformFile = cloudinary.url(saveImage.public_id, {
+      transformation: [...imageQuality]
+    })
 
-    // const { title, transformationType, visibility, authorEditor, tags } = data
-    // const imageModel = await Image.create({
-    //   title,
-    //   transformationType,
-    //   visibility,
-    //   tags: tags.split(','),
-    //   authorEditor: authorEditor ?? user.firstName,
-    //   transformationUrl: null,
-    //   temporalUrlTransformFile,
-    //   author: user,
-    //   publicId: saveImage.public_id,
-    //   bytes: saveImage.bytes,
-    //   width: saveImage.width,
-    //   height: saveImage.height,
-    //   colors: saveImage.colors.flatMap(m => m.join(',')),
-    //   prompt: 'Restore image'
-    // })
-    // if (!imageModel) throw new Error('Error saving image')
+    const { title, transformationType, visibility, authorEditor, tags, author } = data
+    const imageModel = await Image.create({
+      title,
+      transformationType,
+      visibility,
+      tags: tags.split(','),
+      authorEditor,
+      transformationUrl: null,
+      temporalUrlTransformFile,
+      author,
+      publicId: saveImage.public_id,
+      bytes: saveImage.bytes,
+      width: saveImage.width,
+      height: saveImage.height,
+      colors: saveImage.colors.flatMap(m => m.join(',')),
+      prompt: 'Restore image'
+    })
+    if (!imageModel) throw new Error('Error saving image')
     return NextResponse.json({ saveImage })
   } catch (error: any) {
     console.log(error)

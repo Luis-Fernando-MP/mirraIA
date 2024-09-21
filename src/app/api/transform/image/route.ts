3f625688895ed9d3b1252780819946b4487018c4
Luis fernando JU <luigfmp@gmail.com>
2024-09-21 13:05:57 -0500

    test: :test_tube: test code

--- src/app/api/transform/image/route.ts
+++ src/app/api/transform/image/route.ts
@@ -1,45 +1,38 @@
-import Image from '@/db/models/Image'
-import User from '@/db/models/User'
-import cloudinary from '@/shared/cloudinaryConfig'
 import { NextResponse } from 'next/server'
 
 import { ITransformFormData } from './responseCld.type'
 import { uploadCloudinaryBase64 } from './utils/cld'
-import { imageQuality } from './utils/qualitiesTransform'
 
 export async function POST(req: Request) {
   try {
     const reqForm = await req.formData()
     const data: ITransformFormData = Object.fromEntries(reqForm.entries()) as any
 
-    const user = await User.findById({ _id: data.author })
-    if (!user) throw new Error('User not found')
-
     const arrayBuffer = await data.image.arrayBuffer()
     const saveImage = await uploadCloudinaryBase64({ author: data.author, arrayBuffer })
-    const temporalUrlTransformFile = cloudinary.url(saveImage.public_id, {
-      transformation: [...imageQuality]
-    })
+    // const temporalUrlTransformFile = cloudinary.url(saveImage.public_id, {
+    //   transformation: [...imageQuality]
+    // })
 
-    const { title, transformationType, visibility, authorEditor, tags } = data
-    const imageModel = await Image.create({
-      title,
-      transformationType,
-      visibility,
-      tags: tags.split(','),
-      authorEditor: authorEditor ?? user.firstName,
-      transformationUrl: null,
-      temporalUrlTransformFile,
-      author: user,
-      publicId: saveImage.public_id,
-      bytes: saveImage.bytes,
-      width: saveImage.width,
-      height: saveImage.height,
-      colors: saveImage.colors.flatMap(m => m.join(',')),
-      prompt: 'Restore image'
-    })
-    if (!imageModel) throw new Error('Error saving image')
-    return NextResponse.json({ message: 'OK' })
+    // const { title, transformationType, visibility, authorEditor, tags } = data
+    // const imageModel = await Image.create({
+    //   title,
+    //   transformationType,
+    //   visibility,
+    //   tags: tags.split(','),
+    //   authorEditor: authorEditor ?? user.firstName,
+    //   transformationUrl: null,
+    //   temporalUrlTransformFile,
+    //   author: user,
+    //   publicId: saveImage.public_id,
+    //   bytes: saveImage.bytes,
+    //   width: saveImage.width,
+    //   height: saveImage.height,
+    //   colors: saveImage.colors.flatMap(m => m.join(',')),
+    //   prompt: 'Restore image'
+    // })
+    // if (!imageModel) throw new Error('Error saving image')
+    return NextResponse.json({ saveImage })
   } catch (error: any) {
     console.log(error)
     return new Response(`Internal Error: ${String(error.message)}`, { status: 500 })

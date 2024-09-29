0a7f57478dada31e1bbc9df410ddef85ba66bb9d
Luis fernando JU <luigfmp@gmail.com>
2024-09-29 04:32:03 -0500

    ci: :green_heart: fix ci build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     // loader: 'cloudinary',
     domains: ['img.clerk.com', 'res.cloudinary.com', 'i.redd.it']
+  },
+  webpack: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
+      }
+    }
   }
 }
 
